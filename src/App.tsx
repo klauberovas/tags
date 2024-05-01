185ab1ff2eb32c61d539df083a40bbd5ceef476c
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2024-05-01 16:50:21 +0200

    done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { TagCloud } from './components/TagCloud';
+
+function App() {
+  return (
+    <>
+      <TagCloud />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TagCloud/index.tsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import './index.css';
+
+export const TagCloud: React.FC = () => {
+  const [newTag, setNewTag] = useState<string>('');
+  const [tags, setTags] = useState<string[]>([
+    'Workout Gear',
+    'Digital Art',
+    'Replacemnet',
+    'Aria',
+    'Fitness',
+    'Wireless',
+    'iWatch',
+    'Hike',
+    'Sportsband',
+    'Active',
+    'Medical',
+    'Free',
+    'Setup',
+    'Goal',
+    'Black',
+    'Activity',
+    'Dolor',
+  ]);
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setNewTag(e.target.value);
+  };
+
+  const addTag = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    setTags([...tags, newTag]);
+    setNewTag('');
+  };
+
+  const deleteFirstTag = () => {
+    const array: string[] = [...tags];
+    return setTags(array.slice(1));
+  };
+
+  return (
+    <div className="tags">
+      <form onSubmit={addTag} className="tags__form margin--bottom">
+        <label>
+          Add new tag
+          <input
+            onChange={handleInputChange}
+            name="newTag"
+            value={newTag}
+            type="text"
+          />
+        </label>
+        <button type="submit" className="tags__button tags__button--add">
+          Add
+        </button>
+      </form>
+      <div className="tags__cloud">
+        {tags.map((tag, index) => {
+          return (
+            <div className="tag" key={index}>
+              {tag}
+            </div>
+          );
+        })}
+      </div>
+      <button
+        onClick={deleteFirstTag}
+        className="tags__button margin--top tags__button--delete"
+      >
+        Delete first tag
+      </button>
+    </div>
+  );
+};
